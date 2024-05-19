b38cbdd639c5eb475e3baef3ecaaba0969db71e5
Victor Rodriguez <vrodriguez39@live.com.mx>
2024-05-19 01:30:09 -0700

    Tu mensaje de commit

--- database/seeders/Rolseeder.php
+++ database/seeders/Rolseeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
-class Rolseeder extends Seeder
+class RolSeeder extends Seeder
 {
     /**
      * Run the database seeds.
