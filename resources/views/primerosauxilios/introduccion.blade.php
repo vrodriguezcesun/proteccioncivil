1398a65ec648fc19ab0333cfe61102ba63613342
Victor Rodriguez <vrodriguez39@live.com.mx>
2024-05-20 13:38:40 -0700

    Descripción de los cambios realizados

--- resources/views/primerosauxilios/introduccion.blade.php
+++ resources/views/primerosauxilios/introduccion.blade.php
@@ -7,18 +7,19 @@
     <br>
     <main id="main" class="main">
 
-        <h1>Introducción a Primeros Auxilios</h1>
-        <div class="text-center"> <!-- Centramos el contenido -->
-            <div class="embed-responsive embed-responsive-16by9"> <!-- Añadimos la clase para videos responsivos -->
+        <h1 class="text-center">Introducción a Primeros Auxilios</h1>
+        <div class="text-center">
+            <div class="embed-responsive embed-responsive-16by9">
                 <video class="embed-responsive-item" controls>
                     <source src="{{ asset('videos/introduccion1.mp4') }}" type="video/mp4">
                 </video>
             </div>
-            <div class="mt-3"> <!-- Añadimos un margen superior -->
-                <p>Duración: 1.04 min</p>
+            <div class="mt-3">
+                <p class="text-center">Duración: 1.04 min</p>
                 <p>¡Bienvenido! En esta capacitación se muestran los elementos básicos para adquirir y utilizar de forma responsable los instrumentos de primeros auxilios. ¡Te deseamos mucho éxito!</p>
-                <!-- Agregar un botón para ir a la próxima vista -->
-                <a href="{{ url('primerosauxilios/plan') }}" class="btn btn-primary">Siguiente</a>
+                <div class="text-center">
+                    <a href="{{ url('primerosauxilios/plan') }}" class="btn btn-primary">Siguiente</a>
+                </div>
             </div>
         </div>
 
